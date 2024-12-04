aadc2e0336d2157a433872a5ddca4c5b57e4a69c
quietfriend03 <kientrungplus@example.com>
2024-12-04 21:31:33 +0700

    feat: add routing and authentication components, and introduce validation strings

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { Button } from "@/components/ui/button"
-import './App.css'
+import {
+  Navigate,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import { AuthenticationPage } from './auth';
+import { LandingAuthLayout } from './layout/auth-layout';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<LandingAuthLayout />}>
+            {/* Public routes */}
+            {/* <Route index element={<LandingPage />} /> */}
+            <Route path="auth" element={<AuthenticationPage />} />
+
+            {/* Catch-all for unauthenticated 404 */}
+            {/* <Route path="*" element={<Notfoundpage />} /> */}
+        </Route>
+    )
+  );
 
   return (
-      <div>
-      <Button>Click me</Button>
-      </div>
-  )
+    <RouterProvider router = {router}/>
+  );
 }
 
-export default App
+export default App;
