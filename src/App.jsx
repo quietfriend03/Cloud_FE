1c9508404471c65097bd1ca154600304673054cf
quietfriend03 <kientrungplus@example.com>
2024-12-08 14:03:01 +0700

    feat: add input-otp and jwt-decode dependencies, enhance routing structure, and update Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import {
-  Navigate,
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
-import { AuthenticationPage } from './auth';
+import { RequireAuth } from './config/auth_context';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import { LandingAuthLayout } from './layout/auth-layout';
+import { AppLayout } from './layout/app-layout';
+import { LandingPage } from './landing';
+import { AuthenticationPage } from './auth';
+import { DashboardPage } from './dashboard';
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<LandingAuthLayout />}>
-            {/* Public routes */}
-            {/* <Route index element={<LandingPage />} /> */}
-            <Route path="auth" element={<AuthenticationPage />} />
+      <Route path="/" element={<LandingAuthLayout />}>
+        {/* Public routes */}
+        <Route path="" element={<LandingPage />} />
+        <Route path="auth" element={<AuthenticationPage />} />
 
-            {/* Catch-all for unauthenticated 404 */}
-            {/* <Route path="*" element={<Notfoundpage />} /> */}
+        {/* Authenticated routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="/user" element={<AppLayout />}>
+            <Route path="" element={<DashboardPage />} />
+          </Route>
         </Route>
+
+        {/* Catch-all for unauthenticated 404 */}
+        {/* <Route path="*" element={<Navigate to="/auth" />} /> */}
+      </Route>
     )
   );
 
   return (
-    <RouterProvider router = {router}/>
+    <RouterProvider router={router} />
   );
 }
 
